be32b1b70b311aab53797275fca96297828eeffb
Daniel <danielcandidosilwa@gmail.com>
2023-10-10 00:49:29 -0300

    criação das rotas

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 Rails.application.routes.draw do
   resources :orders
   resources :products
-  resources :stores
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
+  get 'stores/:id/products_sales', to: 'stores#products_sales', as: 'store_products_sales'
+  get 'stores/:id/orders', to: 'stores#orders', as: 'store_orders'
+
+  resources :stores do
+    member do
+      get 'orders', to: 'stores#orders'
+    end
+    collection do
+      get 'top_stores', to: 'stores#top_stores'
+    end
+  end
+
   root "home#index"
 end
